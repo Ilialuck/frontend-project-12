23814237b17164e93253e4548030aa9f0bd1bf04
Ilialuck <ilialuck@yandex.ru>
2024-01-17 16:32:31 +0300

    fix lng switcher

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -3,28 +3,25 @@ import {
 } from 'react-bootstrap';
 import { useTranslation } from 'react-i18next';
 import { useAuth } from '../hooks';
-import RuLNG from '../assets/RuLNG.png';
-import EnLNG from '../assets/EnLNG.png';
+// import RuLNG from '../assets/RuLNG.png';
+// import EnLNG from '../assets/EnLNG.png';
 
 const Navbar = () => {
   const auth = useAuth();
   const lngs = {
-    en: { nativeName: 'English' },
-    ru: { nativeName: 'Russian' },
+    en: { nativeName: 'Eng' },
+    ru: { nativeName: 'Ru' },
   };
   const { t, i18n } = useTranslation();
   return (
     <NavbarComponent expand="lg" className="shadow-sm bg-light" id="myclass">
       <Container>
         <NavbarComponent.Brand href="/"> Hexlet Chat </NavbarComponent.Brand>
-        <ButtonGroup>
+        <ButtonGroup className="mb-2">
           {Object.keys(lngs).map((lng) => (
-            <button key={lng} style={{ fontWeight: i18n.resolvedLanguage === lng ? 'bold' : 'normal' }} type="submit" onClick={() => i18n.changeLanguage(lng)}>
-              <img
-                src={lngs[lng].nativeName === 'Russian' ? RuLNG : EnLNG}
-                alt="Button"
-              />
-            </button>
+            <Button key={lng} style={{ fontWeight: i18n.resolvedLanguage === lng ? 'bold' : 'normal' }} type="submit" onClick={() => i18n.changeLanguage(lng)}>
+              {lngs[lng].nativeName}
+            </Button>
           ))}
         </ButtonGroup>
 
